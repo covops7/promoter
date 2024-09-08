ac07c3a6b92728df3e40e7e55cfb9931b117dea1
covops7 <161414185+covops7@users.noreply.github.com>
2024-09-08 23:45:36 +0000

    setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 python -m pip install poetry
 cd promoter
 python -m poetry install
 
 # get 2 images
 curl \
-    -o 'logo/url-transparent-xs.png' \
+    -o '${SCRIPT_DIR}/logo/url-transparent-xs.png' \
     https://imagedelivery.net/stUTp7Ej9_5QujUPLiiNwQ/8c708fcd-3ff8-4981-9c4b-5b777484c900/public
 
 curl \
-    -o 'logo/live-transparent-xs.png' \
+    -o '${SCRIPT_DIR}/logo/live-transparent-xs.png' \
     https://imagedelivery.net/stUTp7Ej9_5QujUPLiiNwQ/de5a944e-cadf-4b05-1d99-6e394b01f800/public
