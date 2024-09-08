12ca1c9d1ed5b79e73ea07849c1c87f977e9b69d
covops7 <161414185+covops7@users.noreply.github.com>
2024-09-08 23:16:02 +0000

    delivery curl scripts

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@
 python -m pip install poetry
 cd app
 python -m poetry install .
+
+# get 2 images
+curl \
+    -o 'logo/url-transparent-xs.png' \
+    https://imagedelivery.net/stUTp7Ej9_5QujUPLiiNwQ/8c708fcd-3ff8-4981-9c4b-5b777484c900/public
+
+curl \
+    -o 'logo/live-transparent-xs.png' \
+    https://imagedelivery.net/stUTp7Ej9_5QujUPLiiNwQ/de5a944e-cadf-4b05-1d99-6e394b01f800/public
